01e618c6e231252476fccf0e488ac6df732d3b26
diogolaureano <diogolaureano.sousa@gmail.com>
2021-08-04 12:31:05 -0300

    Criando servidor http

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 //Importação - Variavel recebendo o modulo da função 
-var calculadora = require("./calculadora")
+//var calculadora = require("./calculadora")
 
 //Para multiplas funções, nome da variavel "." função escolhida
-console.log(calculadora.soma(2, 2))
+//console.log(calculadora.soma(2, 2))
+
+
+//Modulo HTTP ja vem instalado no node
+var http = require("http")
+
+//Servidor http criado
+http.createServer().listen(8181)
+console.log("Meu servidor esta rodando!")
+
+//Instalando Sequelize
