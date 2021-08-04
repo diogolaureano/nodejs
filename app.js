3e7f2d8b04d11216fdf0f7df293f790ea06540a9
diogolaureano <diogolaureano.sousa@gmail.com>
2021-08-04 12:20:35 -0300

    Funcao e modulo

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log("hello World!")
+//Importação - Variavel recebendo o modulo da função 
+var calculadora = require("./calculadora")
+
+//Para multiplas funções, nome da variavel "." função escolhida
+console.log(calculadora.soma(2, 2))
